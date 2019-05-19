cd17ae2122400f190143d3bde59cd921128425d9
Anaxagor <irina-deeva2011.deeva@yandex.ru>
2019-05-19 19:39:38 +0300

    Clustering metrics

--- Wave_wind_model/Wave_wind_model.py
+++ Wave_wind_model/Wave_wind_model.py
@@ -24,9 +24,13 @@ from scipy.cluster import hierarchy
 import random
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
-from statsmodels.tsa.ar_model import AR
+#from statsmodels.tsa.ar_model import AR
 from datetime import datetime
 import math
+from sklearn.manifold import TSNE
+from sklearn.feature_selection import chi2
+from sklearn.feature_selection import SelectKBest
+from sklearn import metrics
 
 
 
@@ -57,10 +61,13 @@ def chunker(seq, size):
 
 
 
-points = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18']
 
 
-size_of_batch = 36
+points = ['1'] #['1','2','3','4','5','6','7','8','9','10','11','12','13',#
+          
+
+
+size_of_batch = 864
 number_of_batches = math.ceil(236664/size_of_batch)
 
 point = []
@@ -77,35 +84,53 @@ for i in range(int(number_of_batches)):
 for j in points:
     y_pred = []
     coef = []
-    coef1 = []
-    coef2 = []
-    coef3 = []
+   
     
+   
+    scaler = MinMaxScaler()
     data = pd.read_fwf(j)
-    columns = data.columns.tolist()
+    columns1 = data.columns.tolist()
 
     mse = []
     #Take wind_speed picks_period avg_period after lasso
-    need_cols = [columns[2],columns[14],columns[15],columns[13]]
-    need_cols_X = [columns[2],columns[14],columns[15]]
-    need_cols_y = [columns[13]]
+    need_cols = [columns1[13],columns1[2],columns1[14],columns1[15]]
+   
+    data = data[need_cols]
     
+    #data = pd.DataFrame(scaler.fit_transform(data))
+    
+    columns1 = data.columns.tolist()
+    need_cols_X = [columns1[1],columns1[2],columns1[3]]
+    need_cols_y = [columns1[0]]
     data_x = data[need_cols_X]
+    
     data_y = data[need_cols_y]
+    #data.columns = ['wind_speed','pick_period','Avg_wave_period','hs']
+    #data.to_csv('data in point'+j+'.csv')
     for i in points:
         if i != j:
             
             data_other = pd.read_fwf(i)
-            columns = data_other.columns.tolist()
-            need_cols = [columns[2],columns[14],columns[15],columns[13]]
+            columns2 = data_other.columns.tolist()
+            need_cols = [columns2[13]]
             df = data_other[need_cols]
             data_x = pd.concat([data_x, df], axis=1)
+            data = pd.concat([data,df], axis = 1)
    
             
     
     
     
     
+   # data.columns = ['hs','ws','picks','period','ws1','hs1','ws2','hs2','ws3','hs3','ws4','hs4','ws5','hs5','ws6','hs6','ws7','hs7','ws8','hs8','ws9','hs9','ws10','hs10','ws11','hs11','ws12','hs12','ws13','hs13','ws14','hs14','ws15','hs15','ws16','hs16','ws17','hs17']
+    data.to_csv('data in point'+j+'.csv')
+    data_norm = pd.DataFrame(scaler.fit_transform(data))
+    
+    
+    data_x_norm = pd.DataFrame(scaler.fit_transform(data_x))
+    data_y_norm = pd.DataFrame(scaler.fit_transform(data_y))
+   
+
     #Autoregression
     #train_data_y = data_y[1:len(data_y)-47333]
     #y_final = []
@@ -116,25 +141,33 @@ for j in points:
    # data_x['wind_dir'] = data_x['wind_dir'].apply(dir)
    # data_x['wave_dir'] = data_x['wave_dir'].apply(dir)
    
-    data_x = data_x.dropna()
-    data_y = data_y.dropna()
+    data_x_norm = data_x.dropna()
+    data_y_norm = data_y.dropna()
    
     #pca = PCA(n_components=4)
     #pca.fit_transform(data_x)
     #scaler = StandardScaler().fit(data_x)
     #scaler.transform(data_x)
     
-    
+    y_mean = pd.DataFrame()
     data_x_split = np.array_split(data_x, number_of_batches)
     data_y_split = np.array_split(data_y, number_of_batches)
+    data_x_norm_split = np.array_split(data_x_norm, number_of_batches)
+    data_y_norm_split = np.array_split(data_y_norm, number_of_batches)
+    coef_frame = pd.DataFrame()
     for batch in range(int(number_of_batches)):
         
 
 
-        Xtrn = data_x_split[batch]
+        Xtrn = data_x_norm_split[batch]
         
-        Ytrn = data_y_split[batch]
-        model_norm = LinearRegression(normalize=True)
+        Ytrn = data_y_norm_split[batch]
+        Xtest = data_x_split[batch]
+        Ytest = data_y_split[batch]
+        model_norm = LinearRegression()
+        df = pd.DataFrame(np.mean(Ytest))
+        y_mean = pd.concat([y_mean,df], axis = 1)
+       
    
         model_norm.fit(Xtrn,Ytrn)
         
@@ -142,25 +175,41 @@ for j in points:
 
         
         
-        mse.append(mean_squared_error(Ytrn,model_norm.predict(Xtrn)))
-        y_pred.append(model_norm.predict(Xtrn))
-        coef.append(model_norm.coef_)
-        coef1.append(model_norm.coef_[0,0])
-        coef2.append(model_norm.coef_[0,1])
-        coef3.append(model_norm.coef_[0,2])
+        mse.append(mean_squared_error(Ytest,model_norm.predict(Xtest)))
+        y_pred.append(model_norm.predict(Xtest))
+        coef.append(model_norm.coef_.tolist())
+        
+        coef_frame = pd.concat([coef_frame,pd.DataFrame(np.transpose(model_norm.coef_))], axis = 1)
    
+    y_mean = y_mean.transpose() 
     batches = []
     for batch in range(int(number_of_batches)):
         batches.append(batch)
-
-    coef_frame = pd.DataFrame([coef1,coef2,coef3])
-   
+    
+        
+    
+    
     coef_frame = coef_frame.transpose()
-    scaler = MinMaxScaler()
-    coef_frame = pd.DataFrame(scaler.fit_transform(coef_frame))
-
-    coef_frame.columns = ['wind_speed_coef','pick_period in_coef','Avg_wave_period_coef']
+    #X = scaler.fit_transform(coef_frame)
+  
+    #coef_frame = pd.DataFrame(X)
     coef_frame.to_csv('coef in point'+j+'.csv')
+    #pca = PCA(n_components=2)
+    #X_pca = pca.fit_transform(coef_frame)
+    #coef_frame = pd.DataFrame(X_pca)
+    X_norm = scaler.fit_transform(coef_frame)
+    coef_frame = pd.DataFrame(X_norm)
+    n_clusters = 3
+    
+    #coef_frame.columns = ['wind_speed coef','pick_period coef','Avg_wave_period coef']
+    #coef_frame.columns = ['wind_speed_coef','pick_period in_coef','Avg_wave_period_coef']
+    #coef_frame.to_csv('PCA coef in point'+j+'.csv')
+    #coef_frame.columns = ['one','two']
+    
+    km = KMeans(n_clusters=n_clusters)
+    #coef_frame['cluster'] = km.fit_predict(X_pca_norm)
+    #km.fit(X_pca_norm)
+    #y_kmeans = km.predict(X_pca_norm)
     
     fig, axs = plt.subplots(4, 1)
     axs[0].plot(chunks,mse)
@@ -168,20 +217,21 @@ for j in points:
     axs[0].set_ylabel('MSE in point ' + j)
     axs[0].grid(True)
    
-
-    axs[1].plot(chunks,coef_frame['wind_speed_coef'].values)
+    
+    axs[1].plot(chunks,X_norm[:,0])
     axs[1].set_xlabel('chunks')
     axs[1].set_ylabel('wind_speed')
     axs[1].grid(True)
 
-    axs[2].plot(chunks,coef_frame['pick_period in_coef'].values)
+    axs[2].plot(chunks,X_norm[:,1])
     axs[2].set_xlabel('chunks')
-    axs[2].set_ylabel('pick_per')
+    axs[2].set_ylabel('picks')
     axs[2].grid(True)
-
-    axs[3].plot(chunks,coef_frame['Avg_wave_period_coef'].values)
+   
+    axs[3].plot(chunks,X_norm[:,2])
+    
     axs[3].set_xlabel('chunks')
-    axs[3].set_ylabel('wave_period')
+    axs[3].set_ylabel('avg_period')
     axs[3].grid(True)
 
     plt.savefig('point '+j+'.png', format='png', dpi=100)
@@ -265,37 +315,38 @@ for j in points:
 
     #coef_frame = pd.DataFrame(ensemble_coef)
 # fit & predict clusters
-    #coef_frame['cluster'] = km.fit_predict(coef_frame)
+    coef_frame['cluster'] = km.fit_predict(coef_frame)
     #km.fit(coef)
     #y_kmeans = km.predict(ensemble_coef)
-    #label = km.labels_
+    label = km.labels_
+    print(metrics.adjusted_rand_score(y_mean.values[:,0], label))
     #data_x.to_csv('clustering.csv')
-    #scatter = dict(
-    #mode = "markers",
-    #name = "y",
-    #type = "scatter3d",    
-    #x = coef_frame[0], y = coef_frame[1], z = coef_frame[2],
-    #marker = dict( size=2, color="rgb(23, 190, 207)" )
-#)
-    #clusters = dict(
-    #alphahull = 7,
-    #name = "y",
-    #opacity = 0.1,
-    #type = "mesh3d",    
-    #x = coef_frame[0], y = coef_frame[1], z = coef_frame[2]
-#)
-    #layout = dict(
-    #title = '3d point clustering',
+    scatter = dict(
+    mode = "markers",
+    name = "y",
+    type = "scatter3d",    
+    x = X_norm[:,0], y = X_norm[:,1], z = X_norm[:,2],
+    marker = dict( size=2, color="rgb(23, 190, 207)" )
+)
+    clusters = dict(
+    alphahull = 7,
+    name = "y",
+    opacity = 0.1,
+    type = "mesh3d",    
+    x = X_norm[:,0], y = X_norm[:,1], z = X_norm[:,2],
+)
+    layout = dict(
+    title = '3d point clustering',
     
     
-    #scene = dict(
-     #   xaxis = dict(title='Auto_regression_coef'),
-      #  yaxis = dict(title='Feature_in_point_coef'),
-       # zaxis = dict(title='Feature_in_other_points_coef'),
-    #)
-#)
- #   fig = dict( data=[scatter, clusters], layout=layout )
-  #  plotly.offline.plot(fig, filename='point_{0}_1989-01-01_00_2015-12-31_23.html'.format(j))
+    scene = dict(
+        xaxis = dict(title='wind_speed'),
+        yaxis = dict(title='picks'),
+        zaxis = dict(title='avg_period'),
+    )
+)
+    fig = dict( data=[scatter, clusters], layout=layout )
+    plotly.offline.plot(fig, filename='point_{0}_1989-01-01_00_2015-12-31_23.html'.format(j))
     
 
 
